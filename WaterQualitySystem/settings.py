bf47c30843e0fe64fb94e77928b624e2490c6ca3
Rudra <rudrasekhri@gmail.com>
2018-09-24 23:34:55 +1000

    Updated static_root

--- WaterQualitySystem/settings.py
+++ WaterQualitySystem/settings.py
@@ -128,6 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
